6fc8cf78bb97229ebf38e6808797e8a9f159e504
Und3fin3d <the.william.greenfield@gmail.com>
2026-07-24 23:30:50 +0100

    firstdigit

--- js/nn-demo.js
+++ js/nn-demo.js
@@ -178,31 +178,27 @@
     [...dots1, ...dots2].forEach(d => (d.style.opacity = "0.08"));
   };
 
-  const drawStarterDigit = () => {
+  const drawStarterDigit = async () => {
     ctx.save();
-    ctx.globalAlpha = 0.68;
-    ctx.lineWidth = 18;
-    ctx.beginPath();
-    ctx.moveTo(102, 91);
-    ctx.quadraticCurveTo(119, 75, 137, 65);
-    ctx.quadraticCurveTo(134, 126, 138, 207);
-    ctx.stroke();
+   
+    const response = await fetch("firstdraw.json");
+    const p = await response.json();
 
-    ctx.lineWidth = 15;
-    ctx.beginPath();
-    ctx.moveTo(104, 216);
-    ctx.quadraticCurveTo(137, 211, 171, 216);
-    ctx.stroke();
 
-    ctx.globalAlpha = 0.13;
-    ctx.lineWidth = 5;
+    let [last, ... points] = p;
     ctx.beginPath();
-    ctx.moveTo(108, 94);
-    ctx.quadraticCurveTo(121, 81, 134, 74);
-    ctx.moveTo(143, 68);
-    ctx.quadraticCurveTo(140, 132, 144, 204);
-    ctx.moveTo(109, 220);
-    ctx.quadraticCurveTo(138, 216, 168, 221);
+    for (const point of points) {
+      ctx.moveTo(last.x, last.y);
+      ctx.lineTo(point.x, point.y);
+      ctx.stroke();
+      last = point;
+    }
+    ctx.moveTo(last.x, last.y);
+    ctx.lineTo(p.x, p.y);
+    console.log(last.x, last.y);
+    ctx.stroke();
+    last = p;
+
     ctx.stroke();
     ctx.restore();
     hint.textContent = "start here: draw a digit, 0–9";
